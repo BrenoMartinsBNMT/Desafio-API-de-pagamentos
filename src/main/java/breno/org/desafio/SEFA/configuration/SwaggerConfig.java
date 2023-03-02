0ff07ca69a16c8b3c0c412ad6a5b64fd2e8ff1cc
Breno Martins <brenomartinscunha@gmail.com>
2023-03-02 14:30:54 -0300

    swagger config

--- src/main/java/breno/org/desafio/SEFA/configuration/SwaggerConfig.java
+++ src/main/java/breno/org/desafio/SEFA/configuration/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package breno.org.desafio.SEFA;
+package breno.org.desafio.SEFA.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
